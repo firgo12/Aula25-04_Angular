60685ed34fa1525ee1a31677f14c92a33be1269d
Logon Aluno <logonrmlocal@fiap.com.br>
2024-04-25 22:21:29 -0300

    first commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,15 @@
+import { ClientDetailComponent } from './components/client-detail/client-detail.component';
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { RotaComponent } from './components/rota/rota.component';
+import { ClienteComponent } from './components/cliente/cliente.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+{ path: '', component: HomeComponent },
+{ path: 'nova-rota', component:RotaComponent },
+{ path: 'cliente/:id', component:ClientDetailComponent },
+{ path: 'cliente', component: ClienteComponent},
+{ path: '**', component: HomeComponent}
+
+
+];
